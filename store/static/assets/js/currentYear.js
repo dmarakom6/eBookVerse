8a1a758ea242cd0be1ba3381b1250311364dd177
Dimitris Marakomihelakis <dmarakom@gmail.com>
2023-08-06 11:10:11 +0300

    clean up

--- store/static/assets/js/currentYear.js
+++
@@ -1,5 +0,0 @@
-
-// Auto year change in copyright text (footer)
-const currentYear = new Date().getFullYear();
-const yearElement = document.getElementById("currentYear");
-yearElement.textContent = currentYear;
--- store/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('books/', views.browse, name='browse'),
-    path('books/<int:book_id>', views.product, name='product'),
-]
