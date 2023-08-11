dcbf1ca90ea1dd29376c1494d3d5491a989a689b
Stelios Karabasakis <stelios@workable.com>
2023-08-11 13:32:04 +0300

    Add description and cover to product page

--- store/urls.py
+++ store/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -6,5 +8,5 @@ urlpatterns = [
     path('books/', views.browse, name='browse'),
     path('books/<int:book_id>', views.product, name='product'),
     path('authors/<int:author_id>', views.author, name='author'),
-    path('publishers/<int:editor_id>', views.editor, name='publisher'),
-]
+    path('publishers/<int:editor_id>', views.editor, name='publisher')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
