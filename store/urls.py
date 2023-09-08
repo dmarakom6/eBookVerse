bb14c7d26ad23217511c0d491fac71e76d1b9cd1
Dimitris Marakomihelakis <dmarakom@gmail.com>
2023-09-08 14:51:00 +0300

    Fix css

--- store/urls.py
+++ store/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('books', views.browse, name='browse'),
+    path('books/', views.browse, name='browse'),
     path('books/<int:book_id>', views.product, name='product'),
     path('authors/<int:author_id>', views.author, name='author'),
     path('publishers/<int:editor_id>', views.editor, name='publisher'),
